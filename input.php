7658fefb41da8192ea6540978670dcdbbb1d444f
root <root@localhost>
2015-11-28 20:19:41 +0530

    Removed unnecessaryfiles

--- input.php
+++
@@ -1,8 +0,0 @@
-<form method=post action=test.php target=_blank>
-Text:<input type=text name=str><br>
-Circle-X:<input type=text name=x><br>
-Circle-Y:<input type=text name=y><br>
-Circle-Z:<input type=text name=z><br>
-Circle-r:<input type=text name=r><br>
-<input type=submit>
-</form>
